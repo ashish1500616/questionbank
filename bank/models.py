372b1e0ee0199183ceccdd9f0802a436b4d6d7ed
Ashish Verma <ashish1500616@gmail.com>
2017-06-26 23:31:43 +0530

    Removed extra files

--- bank/models.py
+++ bank/models.py
@@ -3,50 +3,6 @@ from django.core.urlresolvers import reverse
 from django.conf import settings
 
 
-class Album(models.Model):
-    subject = models.CharField(max_length=250, default=False)
-    semester = models.CharField(max_length=250)
-    branch = models.CharField(max_length=500)
-    concatenate = models.CharField(max_length=100)
-    upload_paper = models.FileField(default=False)
-    a = models.FileField(default=False)
-
-    def get_absolute_url(self):
-        return reverse('bank:detail', kwargs={'pk': self.pk})
-
-    def __str__(self):
-        return self.subject
-
-
-class Song(models.Model):
-    subject = models.ForeignKey(Album, on_delete=models.CASCADE)
-    semester = models.CharField(max_length=10)
-    year = models.CharField(max_length=250)
-    upload_solutions = models.FileField()
-    is_favorite = models.BooleanField(default=False)
-
-    def get_absolute_url(self):
-        return reverse('bank:home',)
-
-    def __str__(self):
-        return self.year
-
-
-class Test(models.Model):
-    subject = models.CharField(max_length=250, default=False)
-    semester = models.CharField(max_length=250)
-    branch = models.CharField(max_length=500)
-    concatenate = models.CharField(max_length=100)
-    upload_paper = models.FileField(default=False)
-    a = models.FileField(default=False)
-
-    def get_absolute_url(self):
-        return reverse('bank:detail', kwargs={'pk': self.pk})
-
-    def __str__(self):
-        return self.branch
-
-
 class Branch(models.Model):
     branch_name = models.CharField(max_length=30)
     branch_image = models.FileField(default=False, blank=True)
--- bank/urls.py
+++ bank/urls.py
@@ -1,51 +1,20 @@
 from django.conf.urls import url
 from . import views
 
-app_name ='bank'
+app_name = 'bank'
 #[-BIT]IT\W\d+\W+
-urlpatterns = [\
-    url(r'^$',views.indexPage,name='indexpage'),
+urlpatterns = [
+    url(r'^$', views.indexPage, name='indexpage'),
+
     url(r'^college/(?P<pk>\d+)/$', views.show_branch, name='branch'),
-    url(r'^college/(?P<cid>\d+)/branch/(?P<bid>\d+)/semester/(?P<sid>\d+)', views.show_subject, name='subject'),
-    url(r'^college/(?P<cid>\d+)/branch/(?P<bbid>\d+)', views.show_sem, name="semester"),
+
+    url(r'^college/(?P<cid>\d+)/branch/(?P<bid>\d+)/semester/(?P<sid>\d+)',
+        views.show_subject, name='subject'),
+    
+    url(r'^college/(?P<cid>\d+)/branch/(?P<bbid>\d+)',
+        views.show_sem, name="semester"),
 
 
-    url(r'^1/$', views.HomeView.as_view(), name='home'),
-
-    url(r'^home2/$', views.Home2View.as_view(), name='home2'),
-
-    url(r'^home1/$', views.Home1View.as_view(), name='home1'),
-
-    url(r'^home3/$', views.Home3View.as_view(), name='home3'),
-
-    url(r'^0/$', views.IndexView.as_view(), name='index'),
-
-    url(r'^2/$', views.Index2View.as_view(), name='index2'),
-
-    url(r'^3/$', views.EceView.as_view(), name='ece'),
-
-    url(r'^4/$', views.Ece2View.as_view(), name='ece2'),
-
-    url(r'^5/$', views.MechView.as_view(), name='mech'),
-
-    url(r'^6/$', views.Mech2View.as_view(), name='mech2'),
-
-
-    url(r'^register/$', views.UserFormView.as_view(), name='register'),
-
-   # url(r'^(?P<pk>[0-9]+)/$', views.DetailView.as_view(), name='detail'),
-
-
-
-    url(r'album/add/$', views.AlbumCreate.as_view(), name='album-add'),
-
-    url(r'song/add/$', views.SongCreate.as_view(), name='song-add'),
-
-
-
-    url(r'album/(?P<pk>[0-9]+)/delete/$', views.AlbumDelete.as_view(), name='album-delete'),
-
-    url(r'album/(?P<pk>[0-9]+)/$', views.AlbumUpdate.as_view(), name='album-update'),
 
 
 ]
